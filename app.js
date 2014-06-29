b6c2d26fc5c7320683c531657549aae37e1f64a9
mosluce <mosluce@gmail.com>
2014-06-29 12:44:16 +0800

    更新： 1. 影片列表 2. 取得串流的路由

--- app.js
+++ app.js
@@ -12,10 +12,34 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
+//影片列表
+app.get('/videos', function (req, res) {
+    var dir = req.param("dir");
+
+    fs.readdir(__dirname + '/videos/' + (dir ? dir + "/" : ""), function (err, files) {
+        var ids = [];
+
+        for(var k in files) {
+            var file = files[k];
+            var split = file.split(/\./);
+            var formats = /mp4/i;
+
+            if(split[1] && formats.test(split[1])) {
+                ids.push(split[0]);
+            }
+        }
+
+        res.json({
+            prefix: '/watch/',
+            ids: ids
+        })
+    });
+});
+
 //讀取影片
-app.get('/videos/:vid', function(req, res) {
+app.get('/watch/:vid', function (req, res) {
     //TODO: 依照傳入的 vid 取得指定的檔案
-    var path = __dirname + '/videos/' + req.param("vid") + '.mp4';
+    var path = __dirname + '/videos/NGNL/' + req.param("vid") + '.mp4';
 
     //開始處理串流
     var stat = fs.statSync(path);
@@ -27,8 +51,8 @@ app.get('/videos/:vid', function(req, res) {
         var partialend = parts[1];
 
         var start = parseInt(partialstart, 10);
-        var end = partialend ? parseInt(partialend, 10) : total-1;
-        var chunksize = (end-start)+1;
+        var end = partialend ? parseInt(partialend, 10) : total - 1;
+        var chunksize = (end - start) + 1;
         console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
 
         var file = fs.createReadStream(path, {start: start, end: end});
